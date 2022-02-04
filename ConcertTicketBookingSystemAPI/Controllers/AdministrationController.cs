03f1f8dbbe9f9167698b480b2e506a3834562850
qwertyuiop1709 <qwertyuiop1709@yandex.ru>
2022-02-04 13:58:49 +0300

    administration

--- ConcertTicketBookingSystemAPI/Controllers/AdministrationController.cs
+++ ConcertTicketBookingSystemAPI/Controllers/AdministrationController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using ConcertTicketBookingSystemAPI.Dtos.AdministrationDtos;
 using Microsoft.AspNetCore.Authorization;
+using ConcertTicketBookingSystemAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConcertTicketBookingSystemAPI.Controllers
 {
@@ -15,23 +17,47 @@ namespace ConcertTicketBookingSystemAPI.Controllers
     public class AdministrationController : ControllerBase
     {
         private readonly ILogger<AdministrationController> _logger;
+        ApplicationContext _context;
 
-        public AdministrationController(ILogger<AdministrationController> logger)
+        public AdministrationController(ILogger<AdministrationController> logger, ApplicationContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpPost]
         [Route("[action]")]
         public async Task<ActionResult> AddAdminAsync(AddAdminDto dto)
         {
-            return Ok();
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == dto.Id);
+            if (user != null)
+            {
+                if (!user.IsAdmin)
+                {
+                    user.IsAdmin = true;
+                    await _context.SaveChangesAsync();
+                    return NoContent();
+                }
+                else return Conflict();
+            }
+            else return NotFound();
         }
         [HttpPost]
         [Route("[action]")]
         public async Task<ActionResult> RemoveAdminAsync(RemoveAdminDto dto)
         {
-            return Ok();
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == dto.Id);
+            if (user != null)
+            {
+                if (user.IsAdmin)
+                {
+                    user.IsAdmin = false;
+                    await _context.SaveChangesAsync();
+                    return NoContent();
+                }
+                else return Conflict();
+            }
+            else return NotFound();
         }
     }
 }
