d575853c32fc1a4d97e8f3ac71673f6a48f2f646
qwertyuiop1709 <qwertyuiop1709@yandex.ru>
2022-04-04 22:26:56 +0300

    fixes, started concertPage

--- ctbs_ReactJs/ctbs-react/src/Elements/App/Personalization/ConcertItem.js
+++ ctbs_ReactJs/ctbs-react/src/Elements/App/Personalization/ConcertItem.js
@@ -3,6 +3,7 @@ import Button from "../../../CommonElements/Button";
 import store from "../../../store";
 import * as actionCreators from "../../../actionCreators";
 import { useLayoutEffect } from "react";
+import { Link } from "react-router-dom";
 
 const widthEEm = () => {
     return window.innerWidth / parseFloat(
@@ -10,6 +11,7 @@ const widthEEm = () => {
             document.querySelector('body')
         )['font-size'])
 }
+const to = concertId => "/Concerts/" + concertId;
 
 function ConcertItem(props) {
     const [widthEm, setWidthEm] = useState(widthEEm());
@@ -21,30 +23,41 @@ function ConcertItem(props) {
         window.addEventListener('resize', settingWidthEm);
         return () => window.removeEventListener('resize', settingWidthEm);
     }, []);
-    return (
-        <div className="ListItem">
-            <div className="isActiveActionList">
+    return (<div className="ListItem">
+        <Link to={to(concert.concertId)} className="isActiveActionList concertLink">
+            <div>
                 {concert.isActiveFlag ? "+" : "-"}
             </div>
-            <div className="concertTypeActionList">
+        </Link>
+        <Link to={to(concert.concertId)} className="concertTypeActionList concertLink">
+            <div >
                 {concert.concertType === 0 ? "Классический" : concert.concertType === 1 ? "Открытое небо" : "Вечеринка"}
             </div>
-            <div className="costActionList">
+        </Link>
+        <Link to={to(concert.concertId)} className="costActionList concertLink">
+            <div >
                 {concert.cost}$
             </div>
-            <div className="leftCountActionList">
+        </Link>
+        <Link to={to(concert.concertId)} className="leftCountActionList concertLink">
+            <div >
                 {concert.leftCount}
             </div>
-            <div className="performerActionList">
+        </Link>
+        <Link to={to(concert.concertId)}  className="performerActionList concertLink">
+            <div>
                 {concert.performer.length * 3 > widthEm ? concert.performer.slice(0, widthEm / 4) + "..." : concert.performer}
             </div>
-            <div className="concertDateActionList">
+        </Link>
+        <Link to={to(concert.concertId)} className="concertDateActionList concertLink">
+            <div >
                 {concert.concertDate}
             </div>
-            <div className="activateButtonActionList">
-                {concert.isActiveFlag ? <Button text="Деактивировать" onClick={deActivateConcert(concert, setConcert)} /> : <Button text="Активировать" onClick={activateConcert(concert, setConcert)} />}
-            </div>
+        </Link>
+        <div className="activateButtonActionList">
+            {concert.isActiveFlag ? <Button text="Деактивировать" onClick={deActivateConcert(concert, setConcert)} /> : <Button text="Активировать" onClick={activateConcert(concert, setConcert)} />}
         </div>
+    </div>
     );
 }
 
