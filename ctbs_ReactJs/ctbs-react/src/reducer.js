ba14d35af8e18f4e79a4b01012fb63948085cc9a
YauhenYN <72869403+YauhenYN@users.noreply.github.com>
2022-06-13 22:11:39 +0300

    Update reducer.js

--- ctbs_ReactJs/ctbs-react/src/reducer.js
+++ ctbs_ReactJs/ctbs-react/src/reducer.js
@@ -1,83 +1,75 @@
 import * as actionTypes from "./actionTypes"
 
 export default function reducer(state, action) {
-    if(action.type === actionTypes.EmptyState){
-        return {
-            ...state, 
-            search: {
-                performer: ""
+    switch (action.type) {
+        case actionTypes.EmptyState:
+            return {
+                ...state,
+                search: {
+                    performer: ""
+                }
             }
-        }
-    }
-    if (action.type === actionTypes.LogIn) { 
-        return {
-            ...state,
-            isLoggedIn: action.isLoggedIn,
-        }
-    }
-    else if (action.type === actionTypes.RefreshCode) {
-        return{
-            ...state
-        }
-    }
-    else if (action.type === actionTypes.GetUserInfo) {
-        return {
-            ...state,
-            user: {
-                userId: action.userId,
-                isAdmin: action.isAdmin,
-                birthDate: action.birthDate,
-                promoCodeId: action.promoCodeId,
-                name: action.name,
-                email: action.email,
-            },
-            cookieConfirmationFlag: action.cookieConfirmationFlag,
-            error: ''
-        }
-    }
-    else if(action.type === actionTypes.ConfirmCookies){
-        return {
-            ...state,
-            cookieConfirmationFlag: true
-        }
-    }
-    else if(action.type === actionTypes.GetUserInfoFailure){
-        return {
-            ...state,
-            error: action.error
-        }
-    }
-    else if (action.type === actionTypes.LogOut) {
-        return{
-            ...state,
-        }
-    }
-    else if(action.type === actionTypes.UpdateBirthDate){
-        return{
-            ...state,
-            birthDateModalDisabled: true,
-            user: {
-                ...state.user,
-                birthDate: action.newBirthDate,
-            },
-        }
-    }
-    else if(action.type === actionTypes.UpdateName){
-        return{
-            ...state,
-            user: {
-                ...state.user,
-                name: action.name
-            },
-        }
-    }
-    else if(action.type === actionTypes.UpdateSearchPerformer){
-        return{
-            ...state,
-            search: {
-                ...state.search,
-                performer: action.performer
+        case actionTypes.LogIn:
+            return {
+                ...state,
+                isLoggedIn: action.isLoggedIn,
             }
-        }
+        case actionTypes.RefreshCode:
+            return {
+                ...state
+            }
+        case actionTypes.GetUserInfo:
+            return {
+                ...state,
+                user: {
+                    userId: action.userId,
+                    isAdmin: action.isAdmin,
+                    birthDate: action.birthDate,
+                    promoCodeId: action.promoCodeId,
+                    name: action.name,
+                    email: action.email,
+                },
+                cookieConfirmationFlag: action.cookieConfirmationFlag,
+                error: ''
+            }
+        case actionTypes.ConfirmCookies:
+            return {
+                ...state,
+                cookieConfirmationFlag: true
+            }
+        case actionTypes.GetUserInfoFailure:
+            return {
+                ...state,
+                error: action.error
+            }
+        case actionTypes.LogOut:
+            return {
+                ...state,
+            }
+        case actionTypes.UpdateBirthDate:
+            return {
+                ...state,
+                birthDateModalDisabled: true,
+                user: {
+                    ...state.user,
+                    birthDate: action.newBirthDate,
+                },
+            }
+        case actionTypes.UpdateName:
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    name: action.name
+                },
+            }
+        case actionTypes.UpdateSearchPerformer:
+            return {
+                ...state,
+                search: {
+                    ...state.search,
+                    performer: action.performer
+                }
+            }
     }
 };
